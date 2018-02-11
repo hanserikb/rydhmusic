c8b588489449f8fc39ef458e5e4ae4a6727fb7c6
Hans Bentlov <hanserikb@gmail.com>
2018-02-11 16:25:41 +0100

    New souds & styles

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const circle = document.querySelector('.circle');
 const sounds = [
-  '/audio/webloop4_tape.m4a',
+  '/audio/webloop-1.mp3',
   '/audio/webloop-2.mp3',
   '/audio/webloop-3.mp3',
   '/audio/webloop-4.mp3',
--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
         // proxy the Webpack Dev Server endpoint
         // (which should be serving on http://localhost:3100/)
         // through BrowserSync
-        proxy: 'http://localhost:9000/'
+        proxy: 'http://localhost:9000'
       },
       // plugin options
       {
