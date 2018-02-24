7e46ddd1160efef78dfcfe5709cb3f0a85b43a5e
Hans Bentlov <hanserikb@gmail.com>
2018-02-24 14:17:19 +0100

    New sounds

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const circle = document.querySelector('.circle');
 const sounds = [
-  '/audio/webloop-1.mp3',
-  '/audio/webloop-2.mp3',
-  '/audio/webloop-3.mp3',
-  '/audio/webloop-4.mp3',
+  '/audio/webloop1.mp3',
+  '/audio/webloop2.mp3',
+  '/audio/webloop3.mp3',
+  '/audio/webloop4.mp3',
+  '/audio/webloop5.mp3',
 ];
 let nowPlayingIndex = 0;
 const myAudio = document.createElement('audio');
